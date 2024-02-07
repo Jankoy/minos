56576c04de9de21f70bbb6c34dd0e07c235adeec
Jankoy <jankoypublic@gmail.com>
2024-02-06 20:26:50 -0500

    Added more functionality to nob.c

--- nob.c
+++ nob.c
@@ -1,6 +1,7 @@
 #define NOB_IMPLEMENTATION
 #include "src/nob.h"
 
+const char *compiler = "cc";
 const char * input_paths[] = {
 	"src/main.c",
 	"src/types.c",
@@ -8,37 +9,48 @@ const char * input_paths[] = {
 	"src/compiler.c",
 	"src/interpreter.c"
 };
+const char *output = "net-test";
 
-int main(int argc, char** argv)
-{
-	NOB_GO_REBUILD_URSELF(argc, argv);
+int main(int argc, char **argv) {
+  NOB_GO_REBUILD_URSELF(argc, argv);
 
-	const char * program = nob_shift_args(&argc, &argv);
-	(void) program;
-	
-	if (nob_needs_rebuild("minos", input_paths, NOB_ARRAY_LEN(input_paths))) {
-		Nob_Cmd cmd = {0};
-		nob_cmd_append(&cmd, "cc");
-		nob_cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb", "-pedantic");
-		nob_cmd_append(&cmd, "-o", "minos");
-		nob_da_append_many(&cmd, input_paths, NOB_ARRAY_LEN(input_paths));
-		if (!nob_cmd_run_sync(cmd)) return 1;	
-	} else {
-		nob_log(NOB_INFO, "Executable is already up to date");
-	}
+  const char *program = nob_shift_args(&argc, &argv);
+  (void)program;
 
-	if (argc > 0) {
-		const char * subcmd = nob_shift_args(&argc, &argv);
+  if (nob_needs_rebuild(output, input_paths, NOB_ARRAY_LEN(input_paths))) {
+    Nob_Cmd cmd = {0};
+    nob_cmd_append(&cmd, compiler);
+    nob_cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb");
+    nob_cmd_append(&cmd, "-o", output);
+    nob_da_append_many(&cmd, input_paths, NOB_ARRAY_LEN(input_paths));
+    if (!nob_cmd_run_sync(cmd))
+      return 1;
+  } else {
+    nob_log(NOB_INFO, "Executable is already up to date");
+  }
 
-		if (strcmp(subcmd, "-r") == 0) {
-			Nob_Cmd cmd = {0};
-			nob_cmd_append(&cmd, "./minos");
-			nob_da_append_many(&cmd, argv, argc);
-			if (!nob_cmd_run_sync(cmd)) return 1;	
-		} else {
-			nob_log(NOB_ERROR, "Unknown subcommand %s", subcmd);
-		}
-	}
-	
-	return 0;
+  if (argc > 0) {
+    const char *subcmd = nob_shift_args(&argc, &argv);
+
+    if (strcmp(subcmd, "-r") == 0) {
+      Nob_Cmd cmd = {0};
+      nob_cmd_append(&cmd, nob_temp_sprintf("./%s", output));
+      nob_temp_reset();
+      nob_da_append_many(&cmd, argv, argc);
+      if (!nob_cmd_run_sync(cmd))
+        return 1;
+    } else if (strcmp(subcmd, "-d") == 0) {
+      Nob_Cmd cmd = {0};
+      nob_cmd_append(&cmd, "gdb");
+      nob_cmd_append(&cmd, nob_temp_sprintf("./%s", output));
+      nob_temp_reset();
+      if (!nob_cmd_run_sync(cmd))
+        return 1;
+    } else {
+      nob_log(NOB_ERROR, "Unknown subcommand %s", subcmd);
+    }
+  }
+
+  return 0;
 }
+
